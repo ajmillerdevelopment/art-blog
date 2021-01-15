204ff7f10ee4280161047162f41cb0467152cb04
Alex <ajm9594@gmail.com>
2021-01-15 13:25:17 -0800

    show users on home page

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express()
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const PORT = process.env.PORT || 4000
+const User = require('./models/User.js')
 
 const userController = require('./controllers/user-controller.js')
 const blogController = require('./controllers/blog-controller.js')
@@ -11,7 +12,11 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(methodOverride('_method'))
 
 app.use('/users', userController);
-app.get('/', (req, res)  => {res.render('home.ejs')})
+app.get('/', (req, res)  => {
+    User.find({}, (err, foundUsers) => {
+        res.render('home.ejs', {users: foundUsers})
+    })
+})
 
 
 app.set('view engine', 'ejs')
