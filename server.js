ee74a86b0f9c7a05203580afae682f41f06d4bc9
Alex <ajm9594@gmail.com>
2021-01-15 11:25:38 -0800

    attached controllers to server

--- server.js
+++ server.js
@@ -4,8 +4,13 @@ const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const PORT = process.env.PORT || 4000
 
+const userController = require('./controllers/user-controller.js')
+const blogController = require('./controllers/blog-controller.js')
+
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(methodOverride('_method'))
+
+app.get('/', (req, res)  => {res.render('index.ejs')})
 
 app.set('view engine', 'ejs')
 app.listen(PORT, console.log(`Listening on ${PORT}`))
