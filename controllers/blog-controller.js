6466e60d676d9612081817d41ab9b15a370d6641
tomjmgray <tomjmgray96@gmail.com>
2021-01-16 13:04:37 -0800

    fixed some minor functionality with edit blog posts

--- controllers/blog-controller.js
+++ controllers/blog-controller.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models');
 
+// Handles user creating a new blog post, renders new blog page
 router.get('/:userId/new', (req, res) => {
     const userId = req.params.userId;
     db.User.findById(userId, (err, foundUser) => {
@@ -13,6 +14,34 @@ router.get('/:userId/new', (req, res) => {
     }); 
 
 
+});
+
+
+// Handles user editing a blog post, render new blog page
+router.get('/:postId/edit', (req, res) => {
+    const postId = req.params.postId;
+    db.Post.findById(postId, (err, foundPost) => {
+        if (err) {
+            return res.send(err);
+        };
+        const context = {
+            post: foundPost
+        };
+        res.render('blog/editBlog', context);
+    })
+})
+
+router.put('/:postId', (req, res) => {
+    const postId = req.params.postId;
+    db.Post.findByIdAndUpdate(postId, {
+        title: req.body.title,
+        body: req.body.body,
+        crosspost: req.body.crosspost
+    }, (err, updatedPost) => {
+        if (err) throw err;
+        console.log(updatedPost);
+        res.redirect(`/`);
+    })
 })
 
 module.exports = router;
