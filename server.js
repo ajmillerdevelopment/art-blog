67333ca4c3d1aef27390e2dadbebec2c97d2e371
Alex <ajm9594@gmail.com>
2021-01-21 09:57:16 -0800

    front page work

--- server.js
+++ server.js
@@ -26,16 +26,12 @@ app.use('/users', userController);
 app.use('/blog', blogController);
 app.get('/', (req, res)  => {
     db.User.find({}, (err, foundUsers) => {
-    db.Image.find({}, (err, foundImages) => {
-    db.Post.findOne({crosspost: true}).populate('author', 'displayName').exec((err, foundPost) => {
-      const context = {users: foundUsers, images: foundImages, post: foundPost}
-      if (req.session.currentUser) {
-        context.currentUser = currentUser;
-    }
-      res.render('home.ejs', context)
-    })
-})
-})});
+        db.Image.find({}, (err, foundImages) => {
+            db.Post.findOne({crosspost: true}).populate('author', 'displayName').exec((err, foundPost) => {
+                res.render('home.ejs', {users: foundUsers, images: foundImages, post: foundPost})    
+            })
+        })
+    })})
 
 // Route for logging out
 
